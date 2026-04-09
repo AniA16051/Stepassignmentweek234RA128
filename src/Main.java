220baec3bb522f8f2be9c38a6ccc60dcb1d05f56
AniA16051 <anirudh16.ashok@gmail.com>
2026-04-09 17:48:41 +0530

    Problem Statement 4- portfolio return sorting

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,28 @@
 /*
-Problem 3: Historical Trade Volume Analysis
-Sort trades by volume using Merge Sort.
-Sort by volume descending using Quick Sort.
-Merge lists and compute total volume.
+Problem 4: Portfolio Return Sorting
+Sort assets using Merge Sort (ascending return).
+Sort using Quick Sort (descending return).
 */
 
-class Trade {
-    String id;
-    int v;
+class Asset {
+    String n;
+    double r;
+    double v;
 
-    Trade(String i, int v) {
-        id = i;
+    Asset(String n, double r, double v) {
+        this.n = n;
+        this.r = r;
         this.v = v;
     }
 }
 
-class P3 {
-    static void merge(Trade[] a, int l, int m, int r) {
+class P4 {
+    static void merge(Asset[] a, int l, int m, int r) {
         int n1 = m - l + 1;
         int n2 = r - m;
 
-        Trade[] L = new Trade[n1];
-        Trade[] R = new Trade[n2];
+        Asset[] L = new Asset[n1];
+        Asset[] R = new Asset[n2];
 
         for (int i = 0; i < n1; i++) L[i] = a[l + i];
         for (int j = 0; j < n2; j++) R[j] = a[m + 1 + j];
@@ -29,7 +30,7 @@ class P3 {
         int i = 0, j = 0, k = l;
 
         while (i < n1 && j < n2) {
-            if (L[i].v <= R[j].v) a[k++] = L[i++];
+            if (L[i].r <= R[j].r) a[k++] = L[i++];
             else a[k++] = R[j++];
         }
 
@@ -37,7 +38,7 @@ class P3 {
         while (j < n2) a[k++] = R[j++];
     }
 
-    static void ms(Trade[] a, int l, int r) {
+    static void ms(Asset[] a, int l, int r) {
         if (l < r) {
             int m = (l + r) / 2;
             ms(a, l, m);
@@ -46,24 +47,24 @@ class P3 {
         }
     }
 
-    static int part(Trade[] a, int l, int h) {
-        int p = a[h].v;
+    static int part(Asset[] a, int l, int h) {
+        double p = a[h].r;
         int i = l - 1;
         for (int j = l; j < h; j++) {
-            if (a[j].v > p) {
+            if (a[j].r > p) {
                 i++;
-                Trade t = a[i];
+                Asset t = a[i];
                 a[i] = a[j];
                 a[j] = t;
             }
         }
-        Trade t = a[i + 1];
+        Asset t = a[i + 1];
         a[i + 1] = a[h];
         a[h] = t;
         return i + 1;
     }
 
-    static void qs(Trade[] a, int l, int h) {
+    static void qs(Asset[] a, int l, int h) {
         if (l < h) {
             int pi = part(a, l, h);
             qs(a, l, pi - 1);
@@ -72,20 +73,15 @@ class P3 {
     }
 
     public static void main(String[] args) {
-        Trade[] a = {
-                new Trade("t1",500),
-                new Trade("t2",100),
-                new Trade("t3",300)
+        Asset[] a = {
+                new Asset("AAPL",12,2),
+                new Asset("TSLA",8,3),
+                new Asset("GOOG",15,1)
         };
 
         ms(a,0,a.length-1);
         qs(a,0,a.length-1);
 
-        int sum = 0;
-        for (Trade t : a) {
-            sum += t.v;
-            System.out.println(t.id + " " + t.v);
-        }
-        System.out.println(sum);
+        for (Asset x : a) System.out.println(x.n + " " + x.r);
     }
 }
