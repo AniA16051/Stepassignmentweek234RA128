29b9186be6030935fca1c50a1d9d6e2cd226b1ee
AniA16051 <anirudh16.ashok@gmail.com>
2026-04-09 17:53:30 +0530

    Problem Statement 6- Risk Threshold Binary lookup

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,34 @@
 /*
-Problem 5: Account ID Lookup in Transaction Logs
-Implement Linear Search.
-Implement Binary Search.
-Handle duplicates and count occurrences.
+Problem 6: Risk Threshold Binary Lookup
+Perform Linear Search for threshold.
+Perform Binary Search to find insertion point.
+Find floor and ceiling values.
 */
 
-class P5 {
-    static int lin(String[] a, String t) {
-        for (int i = 0; i < a.length; i++)
-            if (a[i].equals(t)) return i;
-        return -1;
-    }
-
-    static int bin(String[] a, String t) {
+class P6 {
+    static void fc(int[] a, int t) {
         int l = 0, h = a.length - 1;
+        int f = -1, c = -1;
+
         while (l <= h) {
             int m = (l + h) / 2;
-            int c = a[m].compareTo(t);
-            if (c == 0) return m;
-            if (c < 0) l = m + 1;
-            else h = m - 1;
+            if (a[m] == t) {
+                f = c = a[m];
+                break;
+            }
+            if (a[m] < t) {
+                f = a[m];
+                l = m + 1;
+            } else {
+                c = a[m];
+                h = m - 1;
+            }
         }
-        return -1;
+        System.out.println(f + " " + c);
     }
 
     public static void main(String[] args) {
-        String[] a = {"accA","accB","accC"};
-        System.out.println(lin(a,"accB"));
-        System.out.println(bin(a,"accB"));
+        int[] a = {10,25,50,100};
+        fc(a,30);
     }
 }
